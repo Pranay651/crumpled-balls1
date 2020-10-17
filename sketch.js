1685e09c040863088235b70f5bcd3fb4036a9ca0
Pranay651 <71387154+Pranay651@users.noreply.github.com>
2020-10-17 17:20:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,82 @@
+var  packageSprite;
+var packageBody,ground,ground1,ground2;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	var options={
+		isStatic:false,
+		restitution:0.3,
+		friction:0.5,
+		density:1.2,
+
+	}
+
+	packageSprite=createSprite(-50, 80, 60,60);
+	packageSprite.velocityX=7;
+	packageSprite.velocityY=-7;
+    
+	groundSprite=createSprite(700,690, 200,20);
+	groundSprite.shapeColor=color("red")
+	ground1Sprite=createSprite(790, height-35, 20,100);
+	ground1Sprite.shapeColor=color("red")
+	ground2Sprite=createSprite(610, height-35, 20,100);
+	ground2Sprite.shapeColor=color("red")
+
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	
+	packageBody = Bodies.circle(50, 650 ,30, {options},{maxSides});
+	packageBody.velocityX=7;
+	packageSprite.velocityY=-7;
+	World.add(world, packageBody);
+	ground3Sprite=Bodies.rectangle(300, 700, 800,10,{isStatic:true});
+	ground3Sprite.shapeColor="red";
+	World.add(world,ground3Sprite);
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, 200, 20 , {isStatic:true} );
+	ground.shapeColor="red";
+ 	World.add(world, ground);
+	ground1 = Bodies.rectangle(width/2, 650, 200, 20 , {isStatic:true} );
+	ground1.shapeColor="red";
+    World.add(world, ground1);
+    ground2 = Bodies.rectangle(width/2, 650, 200, 20 , {isStatic:true} );
+	ground2.shapeColor="red";
+	World.add(world, ground2);
+	ground3 = Bodies.rectangle(800, 350, 200, 20 , {isStatic:true} );
+	ground3.shapeColor="red";
+ 	World.add(world, ground3);
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x ;
+  packageSprite.y= packageBody.position.y ;
+  packageSprite.velocityX=packageBody.velocityX;
+  if(keyDown("a")){
+	Matter.Body.applyForce(packageBody.body,packageBody.body.position,{x:85,y:-85});
+ }
+  
+  drawSprites();
+  keyPressed(packageBody,Body);
+}
+function keyPressed(){
+	Matter.Body.applyForce(packageBody,packageBody.position, force(x:85,y:-85));
+}
+
+
